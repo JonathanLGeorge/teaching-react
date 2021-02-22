1488423acd02ecdcf1411cf1ce3ff4879de1458c
Jonathan <jonathan.george.dev@gmail.com>
2021-02-22 15:45:38 -0800

    adding a callback and axios

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import axios from "axios";
 import logo from "./logo.svg";
 import "./App.css";
 import PropsDemo from "./components/PropsDemo";
@@ -5,46 +7,51 @@ import pic1 from "./img/servbots.JPG";
 import pic2 from "./img/servbotdrill.JPG";
 import ClassDemo from "./components/ClassDemo";
 import SearchInput from "./components/SearchInput";
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <PropsDemo
-        name="Jon"
-        date="Today at 4PM"
-        message="React props yeah!"
-        pic={pic1}
-      />
-      <PropsDemo
-        name="Jonathan"
-        date="Today at 6PM"
-        message="Easy right?"
-        pic={pic2}
-      />
-      <br />
-      <div>
-        <ClassDemo />
+class App extends React.Component {
+  onSearchSubmit = (entry) => {
+    console.log("this is our callback returning: " + entry);
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <PropsDemo
+          name="Jon"
+          date="Today at 4PM"
+          message="React props yeah!"
+          pic={pic1}
+        />
+        <PropsDemo
+          name="Jonathan"
+          date="Today at 6PM"
+          message="Easy right?"
+          pic={pic2}
+        />
+        <br />
+        <div>
+          <ClassDemo />
+        </div>
+        <br />
+        <br />
+        <div className="ui container" style={{ marginTop: "30px" }}>
+          <SearchInput onSearchSubmit={this.onSearchSubmit} />
+        </div>
       </div>
-      <br />
-      <br />
-      <div className="ui container" style={{ marginTop: "30px" }}>
-        <SearchInput />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,17 +1,52 @@
 import React, { Component } from "react";
 
 export class SearchInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      entry: "",
+    };
+
+    //this.onFormSubmit = this.onFormSubmit.bind(this) //this is refering to out SearchInput class object
+  }
+
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+
+  // }
+
+  //submitting the form
+  onFormSubmit = (event) => {
+    //this makes it so our form doesnt auto refresh the page on submit
+    event.preventDefault();
+    console.log(this.state.entry); //remembr this is when we submit the form
+
+    //this is our callback function that is inside out App.js
+    //we are passing back our updated state back up to App.js
+    //onSearchSubmit is a props passed in to SearchInput from App.js
+    this.props.onSearchSubmit(this.state.entry);
+  };
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <div className="ui massive icon input">
-              <input type="text" placeholder="search..." />
+              <input
+                type="text"
+                placeholder="search..."
+                onChange={(event) =>
+                  this.setState({ entry: event.target.value.toUpperCase() })
+                }
+                value={this.state.entry}
+              />
               <i className="search icon"></i>
             </div>
           </div>
         </form>
+        {this.state.entry}
       </div>
     );
   }
